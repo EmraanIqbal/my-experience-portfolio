311e0b11325ed755b4ae45b1f80b354dc33087d2
MImranIqbal <se.imraniqbal@gmail.com>
2021-11-02 01:01:21 +0500

    src

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -3,11 +3,10 @@ import { Box, Container, Heading, Spacer, Text } from "@chakra-ui/layout";
 import React from "react";
 import MainProf from "../assets/main.png";
 import "../App.css";
-import { Avatar } from "@chakra-ui/avatar";
 
 const HomeSection = () => {
   return (
-    <Box>
+    <Box ml={{ base: "30px", md: "70px" }} mr={{ base: "30px", md: "70px" }}>
       <Container
         maxW="container.lg"
         border="1px solid white"
@@ -15,15 +14,27 @@ const HomeSection = () => {
         my="50px"
         borderRadius="10"
         background="rgb(250, 202, 246)"
-        h="520px"
+        // h="520px"
+        height={{
+          base: "400px", // 0-30em
+          md: "400px", // 30em-48em
+          lg: "520px", // 48em-62em
+          // "15%", // 62em+
+        }}
       >
         <Box d="flex">
-          <Box mt="150" ml="50">
-            <Text mb="5">Hello, I'm</Text>
+          <Box
+            mt={{ base: "100px", md: "100px" }}
+            ml={{ base: "10px", md: "70px", lg: "100px" }}
+            fontSize={{ base: "10px", md: "15px", lg: "20px" }}
+          >
+            <Text fontWeight="normal" mb="5">
+              Hello, I'm
+            </Text>
             <Heading fontWeight="500" mb="5">
               Emraan Iqbal
             </Heading>
-            <Text mb="5" as="h6" size="xs">
+            <Text fontWeight="normal" mb="5" as="h6" size="xs">
               React Js web developer
             </Text>
           </Box>
@@ -31,12 +42,15 @@ const HomeSection = () => {
           <Box ml="auto" mr="auto">
             <Image
               src={MainProf}
-              borderRadius="full"
+              borderRadius="100%"
+              w="100%"
+              h="100%"
               boxSize="295px"
               objectFit="cover"
               border="7px solid white"
-              mt="100"
+              mt={{ base: "50px", md: "50px", lg: "100px" }}
               mr="100"
+              // h={{ base: "160px", md: "200px", lg: "300px" }}
             />
           </Box>
         </Box>
