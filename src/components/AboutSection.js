6b45d273ba902b85bce2c5f64a444d947aa84186
MImranIqbal <se.imraniqbal@gmail.com>
2021-11-03 08:53:25 -0700

    src

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,10 +8,17 @@ import {
   HStack,
   Text,
 } from "@chakra-ui/layout";
-import React from "react";
+import {
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+} from "@chakra-ui/slider";
+import React, { useState } from "react";
 import MainProf from "../assets/main-profile.png";
 
 const AboutSection = () => {
+  const [expId] = useState(1);
   return (
     <Box
       ml={{ base: "30px", md: "70px" }}
@@ -37,13 +44,15 @@ const AboutSection = () => {
         my="40px"
         borderRadius="20px"
         background="rgb(250, 202, 246)"
+        mb="40px"
         // h="520px"
-        height={{
-          base: "400px", // 0-30em
-          md: "400px", // 30em-48em
-          lg: "700px", // 48em-62em
-          // "15%", // 62em+
-        }}
+        // height={{
+        //   base: "400px", // 0-30em
+        //   md: "400px", // 30em-48em
+        //   lg: "1000px", // 48em-62em
+        //   // "15%", // 62em+
+        // }}
+        h="inherit"
       >
         <Box>
           <Box>
@@ -63,11 +72,11 @@ const AboutSection = () => {
             <Box w="40%">
               <Image
                 m="auto"
-                w="300px"
+                width="332px"
                 objectFit="cover"
                 borderRadius="10px"
                 border="3px solid white"
-                h="400px"
+                h="480px"
                 src={MainProf}
               />
             </Box>
@@ -257,12 +266,15 @@ const AboutSection = () => {
                     variant="outline"
                     border="none"
                     textTransform="capitalize"
-                    color="#011d48"
+                    // color="#011d48"
+                    // isActive={true}
                     mr="20px"
                     fontSize="20px"
                     fontWeight="500"
+                    color="#d45366"
                     p="2"
                     background="transparent"
+                    id="1"
                   >
                     education
                   </Button>
@@ -280,6 +292,127 @@ const AboutSection = () => {
                     experience
                   </Button>
                 </HStack>
+                <Box d="flex">
+                  <Slider
+                    aria-label="slider-ex-3"
+                    defaultValue={100}
+                    fixedValue="50"
+                    focusThumbOnChange={true}
+                    orientation="vertical"
+                    minH="600px"
+                    // w="10px"
+                  >
+                    <SliderTrack>
+                      <SliderFilledTrack bgColor="#d45366" />
+                    </SliderTrack>
+                    <SliderThumb bgColor="#d45366" />
+                  </Slider>
+                  <Box ml="46px" mb="50px" id="1" d="none">
+                    <Heading mt="2" color="#d45366" as="h5" size="sm">
+                      2007-2012
+                    </Heading>
+                    <Box>
+                      <Heading
+                        color="#011d48"
+                        as="h5"
+                        size="sm"
+                        textTransform="capitalize"
+                        d="flex"
+                        alignItems="center"
+                        mt="1"
+                      >
+                        Primary education -
+                        <Heading as="h6" size="xs" ml="1">
+                          islamic ideal school
+                        </Heading>
+                      </Heading>
+                      <Text color="#011d48" my="2">
+                        Primary education is typically the first stage of formal
+                        education, coming preschool/kindergarten and before
+                        secondary school. Primary education takes place in
+                        primary school.
+                      </Text>
+                    </Box>
+                    <Heading mt="2" color="#d45366" as="h5" size="sm">
+                      2012-2017
+                    </Heading>
+                    <Box>
+                      <Heading
+                        color="#011d48"
+                        as="h5"
+                        size="sm"
+                        textTransform="capitalize"
+                        d="flex"
+                        alignItems="center"
+                        mt="1"
+                      >
+                        secondary education -
+                        <Heading as="h6" size="xs" ml="1">
+                          divisional public school (dps)
+                        </Heading>
+                      </Heading>
+                      <Text color="#011d48" my="2">
+                        Secondary education covers two phases on the
+                        International Standard Classification of Education
+                        scale. Level 2 or lower secondary education is
+                        considered the second and final phase of basic
+                        education.
+                      </Text>
+                    </Box>
+                    <Heading mt="3" color="#d45366" as="h5" size="sm">
+                      2017-2020
+                    </Heading>
+                    <Box>
+                      <Heading
+                        color="#011d48"
+                        as="h5"
+                        size="sm"
+                        textTransform="capitalize"
+                        d="flex"
+                        alignItems="center"
+                        mt="1"
+                      >
+                        higher education -
+                        <Heading as="h6" size="xs" ml="1">
+                          unique group of institution
+                        </Heading>
+                      </Heading>
+                      <Text color="#011d48" my="2" justifyContent="center">
+                        Higher education is tertiary education leading to award
+                        of an academic degree. Higher education, also called
+                        post-secondary education, third-level or tertiary
+                        education.
+                      </Text>
+                    </Box>
+                    <Heading mt="3" color="#d45366" as="h5" size="sm">
+                      2020-2024
+                    </Heading>
+                    <Box>
+                      <Heading
+                        color="#011d48"
+                        as="h5"
+                        size="sm"
+                        textTransform="capitalize"
+                        d="flex"
+                        alignItems="center"
+                        mt="1"
+                      >
+                        Bachelor of Computer Science -
+                        <Heading as="h6" size="xs" ml="1">
+                          Government College University Lahore (GCUL)
+                        </Heading>
+                      </Heading>
+                      <Text color="#011d48" my="2" justifyContent="center">
+                        This Institution is one of the oldest seats of learning
+                        in the Muslim world. As a seat of higher learning GC
+                        University blends grand old traditions and modern
+                        educational standards to meet the ideals set by persons
+                        like Dr. Leitner, philosopher the poet, Dr. Muhammad
+                        Iqbal and the Noble Laureate, Dr. Abdus Salam.
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
               </Box>
             </Box>
           </Box>
